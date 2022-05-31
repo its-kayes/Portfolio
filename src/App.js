90760b76ae85145f25df9d61f25dc5429ea9211d
its-kayes <kayes.ek8@gmail.com>
2022-05-31 16:35:22 -0700

    Update Navbar 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
   return (
     <div>
-      <Portfolio> </Portfolio>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Portfolio></Portfolio>}> </Route>
+      </Routes>
     </div>
   );
 }
