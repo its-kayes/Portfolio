645893917cb60d144f3dd174e61a0655dd81e445
its-kayes <kayes.ek8@gmail.com>
2022-07-25 14:20:01 +0600

    Update About Me

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { useState } from 'react';
 import Education from './Education';
 import Overview from './Overview';
-import Review from './Review';
+import Experience from './Experience';
+import Courses from './Courses';
 
 const AboutMe = () => {
 
     let [overview, setOverview] = useState(true);
     let [education, setEducation] = useState(false);
-    let [review, setReview] = useState(false);
-    // let [line, setLine] = useState(false);
+    let [experience, setExperience] = useState(false);
+    let [course, setCourse] = useState(false);
 
     // setLine(<div class="divider">OR</div>)
 
 
 
     return (
-        <div className='bg-[#11213F] text-[#90AFDD] h-screen flex items-center'>
+        <div className='bg-[#0A182E] text-[#90AFDD] h-screen flex items-center'>
             <div className=' flex justify-evenly items-center'>
                 <div className=' w-2/4 text-center '>
                     <i className='text-6xl font-bold text-[#1EC08F]'> About Me </i>
@@ -25,30 +26,41 @@ const AboutMe = () => {
                             <button onClick={() => {
                                 setOverview(true)
                                 setEducation(false)
-                                setReview(false)
-                            }} className={`text-lg cursor-pointer hover:font-bold focus:font-bold  px-8 py-3 my-2 ${overview && 'text-red-500 border-b border-red-400'}`}> <i class="fa-solid fa-eye"></i> Overview </button>
-                        </div>
-
-                        <div>
-                            <button onClick={() => {
-                                setOverview(false)
-                                setEducation(true)
-                                setReview(false)
-                            }} className={`text-lg cursor-pointer hover:font-bold focus:font-bold  px-8 py-3 my-2 ${education && 'text-red-500 border-b border-red-400'}`}><i class="fa-solid fa-graduation-cap"></i> Education </button>
+                                setExperience(false)
+                                setCourse(false)
+                            }} className={`text-lg cursor-pointer hover:font-bold focus:font-bold font-mono px-8 pb-2 my-2 ${overview && 'text-red-500 border-b border-red-400'}`}> <i class="pr-2 fa-solid fa-eye"></i> Overview </button>
                         </div>
 
                         <div>
                             <button onClick={() => {
                                 setOverview(false)
                                 setEducation(false)
-                                setReview(true)
-                                // setLine(true)
-                            }} className={`text-lg cursor-pointer hover:font-bold focus:font-bold  px-8 py-3 my-2 ${review && 'text-red-500 border-b border-red-400'}`}><i class="fa-duotone fa-user-check"></i> Review  </button>
+                                setExperience(false)
+                                setCourse(true)
+                            }} className={`text-lg cursor-pointer hover:font-bold focus:font-bold font-mono px-8 pb-2 my-2 ${course && 'text-red-500 border-b border-red-400'}`}><i class="pr-2 fa-solid fa-crown"></i> Courses </button>
+                        </div>
+
+                        <div>
+                            <button onClick={() => {
+                                setOverview(false)
+                                setEducation(true)
+                                setExperience(false)
+                                setCourse(false)
+                            }} className={`text-lg cursor-pointer hover:font-bold focus:font-bold font-mono px-8 pb-2 my-2 ${education && 'text-red-500 border-b border-red-400'}`}><i class="pr-2 fa-solid fa-graduation-cap"></i> Education </button>
+                        </div>
+                       
+                        <div>
+                            <button onClick={() => {
+                                setOverview(false)
+                                setEducation(false)
+                                setExperience(true)
+                                setCourse(false)
+                            }} className={`text-lg cursor-pointer hover:font-bold focus:font-bold font-mono px-8 pb-2 my-2 ${experience && 'text-red-500 border-b border-red-400'}`}><i class="pr-2 fa-solid fa-crown"></i> Experience </button>
                         </div>
 
                     </div>
                 </div>
-                <div className=' w-2/4 pr-64 '>
+                <div className=' w-2/4 pr-56 border-l pl-20 border-slate-600'>
                     {
                         overview && <Overview />
                     }
@@ -56,7 +68,10 @@ const AboutMe = () => {
                         education && <Education />
                     }
                     {
-                        review && <Review />
+                        experience && <Experience />
+                    }
+                    {
+                        course && <Courses />
                     }
                 </div>
             </div>
--- src/components/about/Education.js
+++ src/components/about/Education.js
@@ -4,9 +4,9 @@ const Education = () => {
     return (
         <div>
             <div>
-                <p className='font-bold'>B.Sc. in Computer Science and Engineering</p>
-                <p className='font-semibold'>Shanto-Mariam University of Creative Technology</p>
-                <p className='my-3'>I have completed my bachelors in Computer Science and Engineering from my hoemcountry. Despite having programming skills from my previous bachelor, I find Web Development more interesting.</p>
+                <p className='font-bold text-xl text-white'>B.Sc. in Computer Science and Engineering</p>
+                <p className='text-white py-1'>Shanto-Mariam University of Creative Technology</p>
+                <p className='my-3'>Currently I'm a student of Last Year. Despite having programming skills from my previous study, I find Web Development more interesting.</p>
             </div>
         </div>
     );
